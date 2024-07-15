ed9928c43bfd1f8f18229a03e434ac40f9fcc477
dung204 <acezombiev4@gmail.com>
2024-07-15 11:43:06 +0700

    feat: update middleware to validate access and refresh tokens

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { millisecondsToSeconds } from 'date-fns';
-import { jwtDecode } from 'jwt-decode';
+import * as jose from 'jose';
 import { NextRequest, NextResponse } from 'next/server';
 import { pathToRegexp } from 'path-to-regexp';
 
@@ -12,45 +11,76 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const redirectUrl = request.nextUrl.clone();
   const accessToken = request.cookies.get('accessToken')?.value;
+  const refreshToken = request.cookies.get('refreshToken')?.value;
   const isAuthRoute = pathname.startsWith('/auth');
   const isPrivateRoute = privateRoutes.some(route =>
     pathToRegexp(route).test(pathname),
   );
+  const jwtAccessSecret = new TextEncoder().encode(
+    process.env['NEXT_PUBLIC_JWT_ACCESS_SECRET']!,
+  );
+  const jwtRefreshSecret = new TextEncoder().encode(
+    process.env['NEXT_PUBLIC_JWT_REFRESH_SECRET']!,
+  );
 
-  if (isAuthRoute && accessToken) {
-    redirectUrl.pathname = '/';
-    return NextResponse.redirect(redirectUrl);
-  }
-
-  if (isPrivateRoute && !accessToken) {
-    redirectUrl.pathname = '/auth/login';
-    return NextResponse.redirect(redirectUrl);
-  }
-
-  if (isPrivateRoute && accessToken) {
-    try {
-      const { exp } = jwtDecode(accessToken);
-      if (exp! < millisecondsToSeconds(Date.now())) {
-        const refreshToken = request.cookies.get('refreshToken')?.value;
-        if (refreshToken) {
-          const res = await axios.post<RefreshSuccessResponse>(
-            `${process.env['NEXT_PUBLIC_API_ENDPOINT']}/api/v1/auth/refresh`,
-            { refreshToken },
-          );
-
-          await axios.post('/api/auth/set-cookie', res.data);
-          return NextResponse.next();
-        }
-        redirectUrl.pathname = '/auth/login';
-        return NextResponse.redirect(redirectUrl);
-      }
-    } catch (error) {
-      redirectUrl.pathname = '/auth/login';
+  if (accessToken && refreshToken) {
+    if (isAuthRoute) {
+      redirectUrl.pathname = '/';
       return NextResponse.redirect(redirectUrl);
     }
+
+    try {
+      await jose.jwtVerify(accessToken, jwtAccessSecret);
+      return NextResponse.next();
+    } catch (accessTokenError) {
+      try {
+        await jose.jwtVerify(refreshToken, jwtRefreshSecret);
+        const response = await axios.post<RefreshSuccessResponse>(
+          `${process.env['NEXT_PUBLIC_API_ENDPOINT']}/auth/refresh`,
+          { refreshToken },
+        );
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+          response.data.data;
+        return NextResponse.redirect(request.nextUrl, {
+          // @ts-ignore
+          headers: {
+            'Set-Cookie': [
+              `accessToken=${newAccessToken}; Path=/; Secure; Max-Age=31536000; HttpOnly; SameSite=Lax`,
+              `refreshToken=${newRefreshToken}; Path=/; Secure; Max-Age=31536000; HttpOnly; SameSite=Lax`,
+            ],
+          },
+        });
+      } catch (refreshTokenError) {
+        redirectUrl.pathname = '/auth/login';
+        return NextResponse.redirect(redirectUrl, {
+          // @ts-ignore
+          headers: {
+            'Set-Cookie': [
+              `accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+              `refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+            ],
+          },
+        });
+      }
+    }
   }
+
+  if (isPrivateRoute) {
+    redirectUrl.pathname = '/auth/login';
+    return NextResponse.redirect(redirectUrl, {
+      // @ts-ignore
+      headers: {
+        'Set-Cookie': [
+          `accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+          `refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+        ],
+      },
+    });
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/auth/(.*)', '/me'],
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 };
