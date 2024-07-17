74b05e71fe1a6882652c559f4dc15b1f595d3d40
dung204 <acezombiev4@gmail.com>
2024-07-17 23:40:26 +0700

    feat: handle borrow book

---
+++ src/common/types/api/checkout.type.ts
@@ -0,0 +1,14 @@
+import { Book } from '@/common/types/api/book.type';
+import { CheckoutStatus } from '@/common/types/api/checkout-status.type';
+import { User } from '@/common/types/api/user.type';
+
+export interface Checkout {
+  id: string;
+  user: User;
+  book: Book;
+  status: CheckoutStatus;
+  createdTimestamp: string;
+  dueTimestamp: string;
+  returnedTimestamp: string | null;
+  note: string | null;
+}
