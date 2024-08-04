1b65901f06611f5820760631751af5fb2b9b89fe
dung204 <acezombiev4@gmail.com>
2024-08-05 02:27:44 +0700

    feat: handle internal global search logic

--- src/containers/global-search.container.tsx
+++ src/containers/global-search.container.tsx
@@ -1,9 +1,26 @@
 'use client';
 
-import { Book, List, MoveRight, Search, UserRoundPen } from 'lucide-react';
+import { AvatarFallback } from '@radix-ui/react-avatar';
+import axios from 'axios';
+import {
+  Book as BookIcon,
+  List,
+  MoveRight,
+  Search,
+  UserPen,
+  UserRoundPen,
+} from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
-import { ComponentProps, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { ComponentProps, useEffect, useState, useTransition } from 'react';
 
+import placeholderImg from '@/assets/images/placeholder-200x300.svg';
+import { Author } from '@/common/types/api/author.type';
+import { Book } from '@/common/types/api/book.type';
+import { Category } from '@/common/types/api/category.type';
+import { SuccessResponse } from '@/common/types/success-response.type';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import {
   Command,
   CommandDialog,
@@ -14,6 +31,7 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from '@/components/ui/command';
+import LoadingIndicator from '@/components/ui/loading-indicator';
 import {
   Tooltip,
   TooltipContent,
@@ -79,88 +97,249 @@ export default function GlobalSearchContainer({
 }
 
 function InternalGlobalSearchContainer({
-  onItemClick: handleCloseDialog,
+  onItemClick,
 }: InternalGlobalSearchContainerProps) {
+  const router = useRouter();
+  const [isSearching, startTransition] = useTransition();
   const [searchQuery, setSearchQuery] = useState('');
+  const [searchedBooks, setSearchedBooks] = useState<Book[]>([]);
+  const [searchedAuthors, setSearchedAuthors] = useState<Author[]>([]);
+  const [searchedCategories, setSearchedCategories] = useState<Category[]>([]);
 
   const handleInputChange = (value: string) => {
     setSearchQuery(value);
+
+    startTransition(() => {
+      (async () => {
+        if (value.length === 0) {
+          setSearchedBooks([]);
+          setSearchedAuthors([]);
+          setSearchedCategories([]);
+          return;
+        }
+
+        const [booksRes, authorsRes, categoriesRes] = await Promise.all([
+          axios.get<SuccessResponse<Book[]>>(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/books?title=${value}`,
+          ),
+          axios.get<SuccessResponse<Author[]>>(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/authors?name=${value}`,
+          ),
+          axios.get<SuccessResponse<Category[]>>(
+            `${process.env.NEXT_PUBLIC_API_ENDPOINT}/categories?name=${value}`,
+          ),
+        ]);
+
+        setSearchedBooks(booksRes.data.data);
+        setSearchedAuthors(authorsRes.data.data);
+        setSearchedCategories(categoriesRes.data.data);
+      })();
+    });
   };
 
-  useEffect(() => {
-    console.log(searchQuery);
-  }, [searchQuery]);
+  const handleRedirectToUrl = (url: string) => {
+    router.push(url);
+    onItemClick?.();
+  };
 
   return (
-    <Command shouldFilter={false} className="rounded-lg border shadow-md">
+    <Command
+      shouldFilter={false}
+      className="rounded-lg border shadow-md outline-none"
+    >
       <CommandInput
         value={searchQuery}
         onValueChange={handleInputChange}
         placeholder="Search by book titles, author names, categories names, ..."
       />
       <CommandList className={cn({ hidden: searchQuery.length === 0 })}>
-        <CommandGroup
-          heading={
-            <div className="flex items-center gap-1">
-              <Book className="h-4 w-4" />
-              Books
-            </div>
-          }
-        >
-          <Link
-            href={`/books?title=${searchQuery}`}
-            onClick={() => handleCloseDialog?.()}
+        {isSearching ? (
+          <CommandItem
+            className={cn(
+              { hidden: searchedBooks.length === 0 },
+              'flex',
+              'justify-center',
+              'items-center',
+              'py-6',
+            )}
           >
-            <CommandItem>
-              See all books containing title &quot;{searchQuery}&quot;
-              <CommandShortcut>
-                <MoveRight className="h-5 w-5" />
-              </CommandShortcut>
-            </CommandItem>
-          </Link>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup
-          heading={
-            <div className="flex items-center gap-1">
-              <UserRoundPen className="h-4 w-4" />
-              Authors
-            </div>
-          }
-        >
-          <Link
-            href={`/authors?name=${searchQuery}`}
-            onClick={() => handleCloseDialog?.()}
-          >
-            <CommandItem>
-              See all authors containing name &quot;{searchQuery}&quot;
-              <CommandShortcut>
-                <MoveRight className="h-5 w-5" />
-              </CommandShortcut>
-            </CommandItem>
-          </Link>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup
-          heading={
-            <div className="flex items-center gap-1">
-              <List className="h-4 w-4" />
-              Categories
-            </div>
-          }
-        >
-          <Link
-            href={`/categories?name=${searchQuery}`}
-            onClick={() => handleCloseDialog?.()}
-          >
-            <CommandItem>
-              See all categories containing name &quot;{searchQuery}&quot;
-              <CommandShortcut>
-                <MoveRight className="h-5 w-5" />
-              </CommandShortcut>
-            </CommandItem>
-          </Link>
-        </CommandGroup>
+            <LoadingIndicator className="h-6 w-6" />
+          </CommandItem>
+        ) : (
+          <>
+            <CommandGroup
+              className={cn({ hidden: searchedBooks.length === 0 })}
+              heading={
+                <div className="flex items-center gap-1">
+                  <BookIcon className="h-4 w-4" />
+                  Books
+                </div>
+              }
+            >
+              {searchedBooks.map(book => (
+                <Link
+                  className="group"
+                  key={book.id}
+                  href={`/book/${book.id}`}
+                  onClick={() => onItemClick?.()}
+                >
+                  <CommandItem
+                    className="group-hover:bg-accent"
+                    value={book.id}
+                    onSelect={() => handleRedirectToUrl(`/book/${book.id}`)}
+                  >
+                    <div className="grid h-12 w-full grid-cols-12 grid-rows-2 gap-x-2">
+                      <div className="relative col-span-1 row-span-2">
+                        <Image
+                          src={book.imageUrl || placeholderImg}
+                          alt={book.title}
+                          fill
+                        />
+                      </div>
+                      <div className="col-span-10 row-span-1 line-clamp-1 text-base font-semibold">
+                        {book.title}
+                      </div>
+                      <div className="col-span-10 row-span-1 line-clamp-1 text-sm">
+                        {book.authors.length > 0 ? (
+                          book.authors.map(author => author.name).join(', ')
+                        ) : (
+                          <span className="italic">Unknown author</span>
+                        )}
+                      </div>
+                    </div>
+                    <CommandShortcut>
+                      <MoveRight className="h-5 w-5" />
+                    </CommandShortcut>
+                  </CommandItem>
+                </Link>
+              ))}
+              <Link
+                className="group"
+                href={`/books?title=${searchQuery}`}
+                onClick={() => onItemClick?.()}
+              >
+                <CommandItem
+                  className="group-hover:bg-accent"
+                  onSelect={() =>
+                    handleRedirectToUrl(`/books?title=${searchQuery}`)
+                  }
+                >
+                  See all books containing title &quot;{searchQuery}&quot;
+                  <CommandShortcut>
+                    <MoveRight className="h-5 w-5" />
+                  </CommandShortcut>
+                </CommandItem>
+              </Link>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup
+              className={cn({ hidden: searchedAuthors.length === 0 })}
+              heading={
+                <div className="flex items-center gap-1">
+                  <UserRoundPen className="h-4 w-4" />
+                  Authors
+                </div>
+              }
+            >
+              {searchedAuthors.map(author => (
+                <Link
+                  className="group"
+                  key={author.id}
+                  href={`/author/${author.id}`}
+                  onClick={() => onItemClick?.()}
+                >
+                  <CommandItem
+                    className="group-hover:bg-accent"
+                    value={author.id}
+                    onSelect={() => handleRedirectToUrl(`/author/${author.id}`)}
+                  >
+                    <Avatar>
+                      {author.imageUrl && <AvatarImage src={author.imageUrl} />}
+                      <AvatarFallback className="flex h-10 w-10 items-center justify-center">
+                        <UserPen />
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="ml-3 text-base font-semibold">
+                      {author.name}
+                    </div>
+                    <CommandShortcut>
+                      <MoveRight className="h-5 w-5" />
+                    </CommandShortcut>
+                  </CommandItem>
+                </Link>
+              ))}
+              <Link
+                className="group"
+                href={`/authors?name=${searchQuery}`}
+                onClick={() => onItemClick?.()}
+              >
+                <CommandItem
+                  className="group-hover:bg-accent"
+                  onSelect={() =>
+                    handleRedirectToUrl(`/authors?name=${searchQuery}`)
+                  }
+                >
+                  See all authors containing name &quot;{searchQuery}&quot;
+                  <CommandShortcut>
+                    <MoveRight className="h-5 w-5" />
+                  </CommandShortcut>
+                </CommandItem>
+              </Link>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup
+              className={cn({ hidden: searchedCategories.length === 0 })}
+              heading={
+                <div className="flex items-center gap-1">
+                  <List className="h-4 w-4" />
+                  Categories
+                </div>
+              }
+            >
+              {searchedCategories.map(category => (
+                <Link
+                  className="group"
+                  key={category.id}
+                  href={`/books?categoryId=${category.id}`}
+                  onClick={() => onItemClick?.()}
+                >
+                  <CommandItem
+                    className="group-hover:bg-accent"
+                    value={category.id}
+                    onSelect={() =>
+                      handleRedirectToUrl(`/books?categoryId=${category.id}`)
+                    }
+                  >
+                    <span className="hidden">{category.id}</span>
+                    <div className="text-base font-semibold">
+                      {category.name}
+                    </div>
+                    <CommandShortcut>
+                      <MoveRight className="h-5 w-5" />
+                    </CommandShortcut>
+                  </CommandItem>
+                </Link>
+              ))}
+              <Link
+                className="group"
+                href={`/categories?name=${searchQuery}`}
+                onClick={() => onItemClick?.()}
+              >
+                <CommandItem
+                  className="group-hover:bg-accent"
+                  onSelect={() =>
+                    handleRedirectToUrl(`/categories?name=${searchQuery}`)
+                  }
+                >
+                  See all categories containing name &quot;{searchQuery}&quot;
+                  <CommandShortcut>
+                    <MoveRight className="h-5 w-5" />
+                  </CommandShortcut>
+                </CommandItem>
+              </Link>
+            </CommandGroup>
+          </>
+        )}
       </CommandList>
     </Command>
   );
