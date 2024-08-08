ce78aed2cdc46d8f32108c382cf72abb94c47bd2
dung204 <acezombiev4@gmail.com>
2024-08-08 13:13:25 +0700

    feat: use category search container in categories page

--- src/containers/book-search.container.tsx
+++ src/containers/book-search.container.tsx
@@ -1,19 +1,16 @@
 'use client';
 
-import { Search } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 import { ComponentProps } from 'react';
 
 import { BookSearchParams } from '@/app/(non-auth)/books/page';
 import { Book } from '@/common/types/api/book.type';
 import { Pagination } from '@/common/types/pagination.type';
 import BookCard from '@/components/ui/book-card';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import BookFilterContainer, {
   BookAdvancedFilterHiddenField,
 } from '@/containers/book-advanced-filter.container';
 import PaginationContainer from '@/containers/pagination.container';
+import SearchBarContainer from '@/containers/search-bar.container';
 
 interface BookSearchContainerProps extends ComponentProps<'div'> {
   books: Book[];
@@ -28,48 +25,20 @@ export default function BookSearchContainer({
   searchParams,
   advancedFilterHiddenFields,
 }: BookSearchContainerProps) {
-  const router = useRouter();
-
-  const handleSearchByTitle = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const title = formData.get('title') as string;
-    const url = new URL(location.href);
-    url.searchParams.set('title', title);
-    router.push(url.toString());
-  };
-
   return (
     <div>
       <section className="flex justify-between">
         <div className="w-1/2">
-          <form
-            className="grid grid-cols-6 gap-4"
-            onSubmit={handleSearchByTitle}
-          >
-            <div className="relative col-span-5">
-              <Input
-                type="text"
-                placeholder="Enter a book title to search..."
-                className="rounded-full ps-10"
-                id="search-bar"
-                name="title"
-              />
-              <label
-                htmlFor="search-bar"
-                className="absolute left-2 top-1/2 -translate-y-1/2"
-              >
-                <Search className="h-6 w-6" />
-              </label>
-            </div>
-            <Button type="submit" className="col-span-1">
-              Search
-            </Button>
-          </form>
-          <p className="mt-3">
-            Showing books containing title:{' '}
-            <b>&quot;{searchParams.title || ''}&quot;</b>
-          </p>
+          <SearchBarContainer
+            fieldName="title"
+            placeholder="Enter a book title to search..."
+          />
+          {searchParams.title && (
+            <p className="mt-3">
+              Showing books containing title:{' '}
+              <b>&quot;{searchParams.title}&quot;</b>
+            </p>
+          )}
         </div>
         <BookFilterContainer
           searchParams={searchParams}
---
+++ src/containers/search-bar.container.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { Search } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { ComponentProps } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+
+interface SearchBarContainerProps
+  extends Omit<ComponentProps<'form'>, 'onSubmit'> {
+  fieldName: string;
+  placeholder?: string;
+}
+
+export default function SearchBarContainer({
+  className,
+  fieldName,
+  placeholder,
+  ...props
+}: SearchBarContainerProps) {
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const fieldValue = formData.get(fieldName) as string;
+    const url = new URL(location.href);
+    url.searchParams.set(fieldName, fieldValue);
+    url.searchParams.set('page', '1');
+    router.push(url.toString());
+  };
+
+  return (
+    <form
+      className={cn('grid grid-cols-6 gap-4', className)}
+      onSubmit={handleSearch}
+      {...props}
+    >
+      <div className="relative col-span-5">
+        <Input
+          type="text"
+          placeholder={placeholder}
+          className="rounded-full ps-10"
+          id="search-bar"
+          name={fieldName}
+        />
+        <label
+          htmlFor="search-bar"
+          className="absolute left-2 top-1/2 -translate-y-1/2"
+        >
+          <Search className="h-6 w-6" />
+        </label>
+      </div>
+      <Button type="submit" className="col-span-1">
+        Search
+      </Button>
+    </form>
+  );
+}
