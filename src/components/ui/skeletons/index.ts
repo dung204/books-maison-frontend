ce266381369ec5f17c64e6fd2d2dea5e52c51a2d
dung204 <acezombiev4@gmail.com>
2024-11-27 16:51:04 +0700

    refactor: skeleton of authors search page

--- src/components/ui/skeletons/index.ts
+++ src/components/ui/skeletons/index.ts
@@ -1,5 +1,5 @@
 export * from './skeleton';
-export * from './authors-grid-skeleton';
+export * from './authors-search-container-skeleton';
 export * from './books-search-container-skeleton';
 export * from './categories-grid-skeleton';
 export * from './data-table-skeleton';
